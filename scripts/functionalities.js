58361f99952e8ad4b95818692fdf64d7527c73ba
Aayush Shukla <aayushshukla11@gmail.com>
2020-02-25 13:55:56 +0530

    background tile fixed

--- scripts/functionalities.js
+++ scripts/functionalities.js
@@ -1,30 +1,28 @@
 var scrollTimer = -1;
-var oldScroll=0;
+var oldScroll = 0;
 var isRight;
-var positionM=0;
-window.onscroll = function(e) {
+var positionM = 0;
+window.onscroll = function (e) {
   // print "false" if direction is down and "true" if up
-  if(this.oldScroll > this.scrollY)
-  {
-    document.getElementById("mario").className="marioRunL";
+  if (this.oldScroll > this.scrollY) {
+    document.getElementById("mario").className = "marioRunL";
   }
-  else
-  {
-    document.getElementById("mario").className="marioRunR";
+  else {
+    document.getElementById("mario").className = "marioRunR";
   }
-  if (scrollTimer != -1){                 
-       clearTimeout(scrollTimer);
+  if (scrollTimer != -1) {
+    clearTimeout(scrollTimer);
   }
-      //position of 
-    positionM=parseInt((scrollY/(document.body.scrollHeight-document.body.clientHeight))*100);
-    document.getElementById("mario").style.left=positionM+"%";
-    //console.log(positionM);
+  //position of 
+  positionM = parseInt((scrollY / (document.body.scrollHeight - document.body.clientHeight)) * 100);
+  document.getElementById("mario").style.left = positionM + "%";
+  //console.log(positionM);
 
-    scrollTimer = window.setTimeout("scrollFinished()", 350);
+  scrollTimer = window.setTimeout("scrollFinished()", 350);
   this.oldScroll = this.scrollY;
 }
 function scrollFinished() {
-   document.getElementById("mario").className="mariofaceR";
+  document.getElementById("mario").className = "mariofaceR";
 }
 //add code for left and right on scroll up and down
 //document.getElementById("mario").classList.add("marioRun");
@@ -39,64 +37,60 @@ var isInViewport = function (elem) {
   //is section displaying in viewport works now
 };
 function getScrollbarWidth() {
-    // Creating invisible container
-    const outer = document.createElement('div');
-    outer.style.visibility = 'hidden';
-    outer.style.overflow = 'scroll'; // forcing scrollbar to appear
-    outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
-    document.body.appendChild(outer);
-    // Creating inner element and placing it in the container
-    const inner = document.createElement('div');
-    outer.appendChild(inner);
-    // Calculating difference between container's full width and the child width
-    const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
-    // Removing temporary elements from the DOM
-    outer.parentNode.removeChild(outer);
-    return scrollbarWidth;
-    }
+  // Creating invisible container
+  const outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+  document.body.appendChild(outer);
+  // Creating inner element and placing it in the container
+  const inner = document.createElement('div');
+  outer.appendChild(inner);
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+  return scrollbarWidth;
+}
 
 
-    $(window).scroll(function(){
-      var scroll = $(window).scrollTop(),
-      dh=$(document).height(),
-      wh=$(window).height(),
-      //value=(scroll/(dh-wh)) * 100;
-      value=(scrollY/(document.body.scrollHeight-document.body.clientHeight))*100;
-     console.log(value);
-      $('#container').css('left',-value * 6  + '%');
-      containerSize=parseFloat($('#container').css('left'));
-      if(parseInt(Math.round(value))==100){
-        console.log("loading extra space");
-        $('#container').css('left',containerSize-(6*getScrollbarWidth()));
-      }
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop(),
+    dh = $(document).height(),
+    wh = $(window).height(),
+    //value=(scroll/(dh-wh)) * 100;
+    value = (scrollY / (document.body.scrollHeight - document.body.clientHeight)) * 100;
+  console.log(value);
+  $('#container').css('left', -value * 6 + '%');
+  containerSize = parseFloat($('#container').css('left'));
+  if (parseInt(Math.round(value)) == 100) {
+    console.log("loading extra space");
+    $('#container').css('left', containerSize - (6 * getScrollbarWidth()));
+  }
 
-    //  console.log(containerSize);
-    })
-    function scrolltoY(num)
-    {
-      var toSubtract=getScrollbarWidth()*num;
-      var bruh=100/6;
-      num=num*bruh;
-      console.log(num);
-      var bleh=document.body.scrollHeight-document.body.clientHeight;
-      var scrollAm=(num*bleh)/100;
-      window.scrollTo(0,scrollAm+toSubtract);
-    }
+  //  console.log(containerSize);
+})
+function scrolltoY(num) {
+  var toSubtract = getScrollbarWidth() * num;
+  var bruh = 100 / 6;
+  num = num * bruh;
+  console.log(num);
+  var bleh = document.body.scrollHeight - document.body.clientHeight;
+  var scrollAm = (num * bleh) / 100;
+  window.scrollTo(0, scrollAm + toSubtract);
+}
 
-    function changeYPos()
-    {
-      var marioPos=parseInt($("#mario").position().top)-51;
-      marioPos=marioPos+"px";
-      console.log(marioPos);
-      const demoClasses = document.querySelectorAll('.bottomTile');
-      demoClasses.forEach(element => {
-        element.style.top=marioPos;
-      });
-      const bruhClasses = document.querySelectorAll('.UbottomTile');
-      bruhClasses.forEach(element => {
-        element.style.top=marioPos;
-      });
-      set
-    }
-    changeYPos();
-    setInterval(changeYPos,1000);
+function changeYPos() {
+  var marioPos = parseInt($("#mario").position().top) - 51;
+  marioPos = marioPos + "px";
+  console.log(marioPos);
+  const demoClasses = document.querySelectorAll('.bottomTile');
+  demoClasses.forEach(element => {
+    element.style.top = marioPos;
+  });
+  const bruhClasses = document.querySelectorAll('.UbottomTile');
+  bruhClasses.forEach(element => {
+    element.style.top = marioPos;
+  });
+  set
+}
